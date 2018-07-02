696e7ce55f06098c5f68f0e1553629ff7fae8490
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-07-02 15:16:00 +0200

    things

--- src/sea.glsl
+++ src/sea.glsl
@@ -14,7 +14,7 @@ vec3 colorB = vec3(0.0,0.0,0.7);
 void main(){
     vec2 uv = gl_FragCoord.xy / resolution.xy;
     //vec2 mv = vec2(mX/resolution.x, 1-(mY/resolution.y));
-    float d = distance(vec2(uv.x,uv.y), vec2(.5,.8))*3;
+    float d = distance(vec2(uv.x,uv.y), vec2(.5,.8))*3.;
     vec3 color = mix(colorA, colorB, d);
     vec3 final = mix(color, vertColor.xyz, .5);
     gl_FragColor = vec4(final, 0.8);
