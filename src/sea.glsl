e089af1cd00a8b2a1e5069e744159241f7eca52c
Krabcode <jakubpodtrzitkorak@gmail.com>
2018-07-05 23:28:17 +0200

    impl FFT

--- src/sea.glsl
+++ src/sea.glsl
@@ -1,22 +1,29 @@
+
 #ifdef GL_ES
-precision highp float;
-precision highp int;
+precision mediump float;
+precision mediump int;
 #endif
+
 uniform vec2 resolution;
 uniform float time;
 uniform float mX;
 uniform float mY;
-varying vec4 vertColor;
 
 const vec3 colorA = vec3(1.0,0.,0.);
 const vec3 colorB = vec3(0.005,0.005,0.09);
 
+
+uniform sampler2D texture;
+
+varying vec4 vertColor;
+varying vec4 vertTexCoord;
+
 void main(){
     vec2 uv = gl_FragCoord.xy / resolution.xy;
     //vec2 mv = vec2(mX/resolution.x, 1-(mY/resolution.y));
-//    float d = distance(vec2(uv.x,uv.y), vec2(.5,.8))*3.;
-//    vec3 color = mix(colorA, colorB, d);
-    vec3 red = vec3(1.,0.,0.);
-    vec3 lit = mix(colorB, red, (vertColor.x+vertColor.y+vertColor.z));
-    gl_FragColor = vec4(lit, .7);
+    //float d = distance(vec2(uv.x,uv.y), vec2(.5,.8))*3.;
+    //vec3 color = mix(colorA, colorB, d);
+//     vec3 red = vec3(1.,0.,0.);
+//     vec3 lit = mix(colorB, (texture2D(texture, vertTexCoord.st) * vertColor).xyz, .5);
+    gl_FragColor = vec4((texture2D(texture, vertTexCoord.st) * vertColor).xyz, .6);
 }
